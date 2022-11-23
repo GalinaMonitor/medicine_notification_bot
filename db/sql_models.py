6a4968f731229bfcc1dfb9967342b95b408d5c9c
gmonitor <seregin.da@didenok.com>
2022-11-23 17:36:00 +0300

    fix: сделать primary key bigint

--- db/sql_models.py
+++ db/sql_models.py
@@ -1,20 +1,25 @@
 from datetime import date
 from typing import Optional
 
+from sqlalchemy import Column, BigInteger
 from sqlmodel import SQLModel, Field, create_engine
 
 from config import settings
 
 
 class Doctor(SQLModel, table=True):
-	id: Optional[int] = Field(default=None, primary_key=True)
+	id: Optional[int] = Field(
+		sa_column=Column(BigInteger(), default=None, primary_key=True)
+	)
 	fullname: str
 	access_opp: Optional[bool] = Field(default=True)
 	access_gepa_merz: Optional[bool] = Field(default=False)
 
 
 class Patient(SQLModel, table=True):
-	history_number: Optional[int] = Field(default=None, primary_key=True)
+	history_number: Optional[int] = Field(
+		sa_column=Column(BigInteger(), default=None, primary_key=True)
+	)
 	fullname: str
 	admission_date: Optional[date] = Field(default=None)
 	discharge_date: Optional[date] = Field(default=None)
