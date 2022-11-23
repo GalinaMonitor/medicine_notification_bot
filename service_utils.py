6845e737364439d9bff91b510b738b567ce57028
gmonitor <seregin.da@didenok.com>
2022-11-23 10:52:02 +0300

    feat: более читабельные уведомления

--- service_utils.py
+++ service_utils.py
@@ -10,25 +10,25 @@ def check_notifications_opp(patient: Patient, checked_date: date) -> List[str]:
 	if patient.is_opp:
 		if checked_date - patient.admission_date == timedelta(days=1):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - ОПП.Повторные анализы'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - ОПП.Повторные анализы'
 			)
 		if checked_date - patient.admission_date == timedelta(days=5):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - ОПП.Повторные анализы'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - ОПП.Повторные анализы'
 			)
 		if checked_date - patient.discharge_date == timedelta(weeks=12):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - ОПП.Повторный визит'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - ОПП.Повторный визит'
 			)
 
 	if not patient.is_opp:
 		if checked_date - patient.admission_date == timedelta(days=7):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - БЕЗ ОПП.Повторные анализы'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - БЕЗ ОПП.Повторные анализы'
 			)
 		if checked_date - patient.discharge_date == timedelta(days=12):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - БЕЗ ОПП.Повторный визит'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - БЕЗ ОПП.Повторный визит'
 			)
 
 	return notification_list
@@ -40,11 +40,11 @@ def check_notifications_gepa_merz(patient: Patient, checked_date: date) -> List[
 	if patient.is_gepa_merz:
 		if checked_date - patient.admission_date == timedelta(weeks=4):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - Гепа-мерц.Повторный визит'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - Гепа-мерц.Повторный визит'
 			)
 		if checked_date - patient.discharge_date == timedelta(weeks=10):
 			notification_list.append(
-				f'{checked_date.strftime("%d-%m-%y")}: {patient.fullname} - Гепа-мерц.Повторный визит'
+				f'{checked_date.strftime("%d-%m-%y")}: {patient.history_number} {patient.fullname} - Гепа-мерц.Повторный визит'
 			)
 
 	return notification_list
